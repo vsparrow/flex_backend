9067d40175652a7f574e974f3c550e005dfebe7f
vsparrow <khan@vsparrow.com>
2018-04-17 11:44:21 -0400

    Api now sending description of item

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,5 +1,5 @@
 class ItemSerializer < ActiveModel::Serializer
-  attributes :id, :title, :brand, :image, :size, :price#, :description
+  attributes :id, :title, :brand, :image, :size, :price, :description
   belongs_to :category
 end
 
